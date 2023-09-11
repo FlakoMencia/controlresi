346c67974213aafdb6aaa5967044d63ca8f95a42
Flako Mencia <mario_mencia@hotmail.com>
2023-09-10 22:59:46 -0600

    Initial commit

---
+++ src/test/java/sv/com/control/residential/controlresi/ControlresiApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.com.control.residential.controlresi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlresiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
